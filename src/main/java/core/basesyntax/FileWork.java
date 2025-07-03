9115604a3974940f5bfae3e3c55bb4203125586e
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-07-04 01:08:19 +0300

    Update FileWork.java

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            StringBuilder builder = new StringBuilder();
+            int value = reader.read();
+            while (value != -1) {
+                builder.append((char) value);
+                value = reader.read();
+            }
+            reader.close();
+
+            String resultString = builder.toString();
+            String[] words = resultString.split(" ");
+            List<String> resultList = new ArrayList<>();
+            for (String word: words) {
+                if (word.startsWith("w") || word.startsWith("W")) {
+                    resultList.add(word);
+                }
+            }
+            return resultList.toArray(new String[0]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
