dac1f4c21a5f9554be29aaada138a79cc26ecef2
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-07-04 13:57:01 +0300

    Update FileWork.java

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
