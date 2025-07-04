bb864c6ee613a780d4778a5db5c006e758db73e1
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-07-04 10:48:20 +0300

    Update Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
-public static void main(String[] args) {
-    FileWork filework = new FileWork();
-    String[] result = filework.readFromFile("File.txt");
-    System.out.println(Arrays.toString(result));
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWork filework = new FileWork();
+        String[] result = filework.readFromFile("File.txt");
+        System.out.println(Arrays.toString(result));
+    }
 }
