cdbcabb4625eb3293dd8882e9251227e8cb2010a
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-07-04 13:59:24 +0300

    Update FileWork.java

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileWork {
